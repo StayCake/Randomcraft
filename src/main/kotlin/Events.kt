88cec8922d037ed5253f6c55476865e3a6bab213
keikoi <admin@koisv.com>
2021-11-14 17:57:50 +0900

    Item Drop Randomized [0.47Snap]

--- src/main/kotlin/Events.kt
+++ src/main/kotlin/Events.kt
@@ -1,32 +1,43 @@
+import org.bukkit.Material
+import org.bukkit.entity.Item
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockDropItemEvent
+import org.bukkit.event.entity.ItemSpawnEvent
 import org.bukkit.event.inventory.CraftItemEvent
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.inventory.InventoryType
 import org.bukkit.event.inventory.SmithItemEvent
 import org.bukkit.inventory.AnvilInventory
 import org.bukkit.inventory.ItemStack
-import org.bukkit.inventory.SmithingRecipe
 import org.bukkit.inventory.meta.Damageable
 import java.util.*
 
 class Events : Listener {
     @EventHandler
+    fun mine(e: ItemSpawnEvent) {
+        val materialList = Material.values().toMutableList()
+        materialList.filter { !it.isAir }
+        val itemSel = materialList[Random().nextInt(materialList.size)]
+        e.entity.itemStack = ItemStack(itemSel,Random().nextInt(itemSel.maxStackSize))
+    }
+
+    @EventHandler
     fun pick(e: CraftItemEvent) {
-        println(e.inventory.result)
         val result = e.inventory.result
+        if (result?.type?.maxDurability?.toInt() == 0) return
         if (result?.itemMeta is Damageable) {
-            if (e.isShiftClick || e.isRightClick) e.isCancelled = true
+            if ((e.isShiftClick || e.isRightClick)) e.isCancelled = true
             e.inventory.result = getResult(result)
         }
     }
 
     @EventHandler
     fun smith(e: SmithItemEvent) {
-        println(e.inventory.result)
         val result = e.inventory.result
+        if (result?.type?.maxDurability?.toInt() == 0) return
         if (result?.itemMeta is Damageable) {
-            if (e.isShiftClick || e.isRightClick) e.isCancelled = true
+            if ((e.isShiftClick || e.isRightClick)) e.isCancelled = true
             e.inventory.result = getResult(result)
         }
     }
@@ -38,7 +49,7 @@ class Events : Listener {
                 val anvil = e.inventory as AnvilInventory
                 val result = anvil.result
                 if (result?.itemMeta is Damageable) {
-                    if (e.isShiftClick || e.isRightClick) e.isCancelled = true
+                    if ((e.isShiftClick || e.isRightClick) && result.type.maxDurability.toInt() != 0) e.isCancelled = true
                     anvil.result = getResult(result)
                 }
             }
